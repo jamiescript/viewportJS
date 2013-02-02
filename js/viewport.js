6bbb2cc4cb922e98fb68c1a012a4d3def1d069c4
Jamie Piper <jamie.piper@alliants.co.uk>
2013-02-02 18:34:46 +0000

    initial commit

---
+++ js/viewport.js
@@ -0,0 +1,44 @@
+/*
+  viewport.js - development utility plugin for testing various device viewports
+*/
+
+var url = window.location.pathname,
+    deviceList = document.createElement('div');
+    deviceList.className = 'button-group';
+    deviceList.innerHTML = '<button class="js-iphone-portrait">Iphone Portrait</button><button class="js-iphone-landscape">Iphone Landscape</button><button class="js-ipad-portrait">Ipad Portrait</button><button class="js-ipad-landscape">Ipad Landscape</button><button class="js-device-slider device-icon">device?</button>';
+
+document.body.appendChild(deviceList);
+
+document.addEventListener('click', function(e) {
+  var matches;
+  (function(doc) {
+    matches = doc.matchesSelector ||
+              doc.webkitMatchesSelector ||
+              doc.mozMatchesSelector ||
+              doc.oMatchesSelector ||
+              doc.msMatchesSelector;
+  })(document.documentElement);
+
+  if ( matches.call( e.target, 'button') ) {
+    var device = e.target.className;
+    switch(device) {
+    case ('js-iphone-portrait'):
+      w = window.open(url,'', 'width=320,height=480');
+      break;
+    case ('js-iphone-landscape'):
+      w = window.open(url,'', 'width=480,height=320');
+      break;
+    case ('js-ipad-portrait'):
+      w = window.open(url,'', 'width=768,height=1024');
+      break;
+    case ('js-ipad-landscape'):
+      w = window.open(url,'', 'width=1024,height=768');
+      break;
+    default:
+      if (e.target.className.indexOf('device-list-reveal') === -1) {
+        e.target.parentNode.className = 'button-group device-list-reveal';
+      }
+      // TODO remove device-list-reveal class name
+    }
+  }
+}, false);

7a7b741c65f590a70a94002d379efaff55014e61
Jamie Piper <jamie.piper@alliants.co.uk>
2013-02-03 11:40:36 +0000

    refactor js

--- js/viewport.js
+++ js/viewport.js
@@ -5,7 +5,13 @@
 var url = window.location.pathname,
     deviceList = document.createElement('div');
     deviceList.className = 'button-group';
-    deviceList.innerHTML = '<div class="device-list"><button class="js-iphone-portrait">Iphone Portrait</button><button class="js-iphone-landscape">Iphone Landscape</button><button class="js-ipad-portrait">Ipad Portrait</button><button class="js-ipad-landscape">Ipad Landscape</button></div><button class="js-device-slider device-icon">device?</button>';
+    deviceList.innerHTML = '<div class="device-list">\
+    <button class="js-iphone-portrait">Iphone Portrait</button>\
+    <button class="js-iphone-landscape">Iphone Landscape</button>\
+    <button class="js-ipad-portrait">Ipad Portrait</button>\
+    <button class="js-ipad-landscape">Ipad Landscape</button>\
+    </div>\
+    <button class="js-device-slider device-icon">device?</button>';
 
 document.body.appendChild(deviceList);
 
@@ -19,23 +25,22 @@ document.addEventListener('click', function(e) {
               doc.msMatchesSelector;
   })(document.documentElement);
 
-  if ( matches.call( e.target, 'button') ) {
-    var device = e.target.className;
-    switch(device) {
+  if ( matches.call(e.target, 'button') ) {
+    switch(e.target.className) {
     case ('js-iphone-portrait'):
-      w = window.open(url,'', 'width=320,height=480');
+      window.open(url,'', 'width=320,height=480');
       break;
     case ('js-iphone-landscape'):
-      w = window.open(url,'', 'width=480,height=320');
+      window.open(url,'', 'width=480,height=320');
       break;
     case ('js-ipad-portrait'):
-      w = window.open(url,'', 'width=768,height=1024');
+      window.open(url,'', 'width=768,height=1024');
       break;
     case ('js-ipad-landscape'):
-      w = window.open(url,'', 'width=1024,height=768');
+      window.open(url,'', 'width=1024,height=768');
       break;
     default:
-      e.target.previousSibling.style.display='block';
+      e.target.previousSibling.previousSibling.style.display='block';
     }
   }
 }, false);
